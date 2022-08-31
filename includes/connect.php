fffa4fc0e9e8949deeb6e41f40e8d4365865971c
Aminul Islam Joy <60224967+Joy602@users.noreply.github.com>
2022-08-31 15:52:30 +0600

    Initial commit

---
+++ includes/connect.php
@@ -0,0 +1,12 @@
+<!-- Database Connection -->
+
+<?php
+
+$conn = mysqli_connect('localhost','root','','mystore');
+if(!$conn)
+{
+    // echo "Connection Successful!!";
+    die(mysqli_error($conn));
+} 
+
+?>
